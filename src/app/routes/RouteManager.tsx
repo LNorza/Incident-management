4a085ab3d00868e9d206b79ee6e1c86028cae7cb
LNorza <luiscnorza@gmail.com>
2024-09-13 20:57:57 -0700

    add Api

--- src/app/routes/RouteManager.tsx
+++ src/app/routes/RouteManager.tsx
@@ -1,21 +1,24 @@
-import {Navigate, Route, Routes} from "react-router-dom";
-import {BuildingPage} from "../pages/BuildingPage";
-import {Navbar, Sidebar} from "../../ui";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { BuildingPage } from "../pages/BuildingPage";
+import { Navbar, Sidebar } from "../../ui";
+import { Dashboard } from "../pages";
+import style from "../style/appContainer.module.css";
 
 export const RouteManager = () => {
-	return (
-		<>
-			<Sidebar />
-			<Navbar />
+    return (
+        <>
+            <Sidebar />
+            <Navbar />
 
-			<div className="container">
-				<Routes>
-					<Route path="build" element={<BuildingPage />} />
+            <div className={`${style.container}`}>
+                <Routes>
+                    <Route path="home" element={<Dashboard />} />
+                    <Route path="build" element={<BuildingPage />} />
 
-					{/* DefaultRoute */}
-					<Route path="/" element={<Navigate to={"/build"} />} />
-				</Routes>
-			</div>
-		</>
-	);
+                    {/* DefaultRoute */}
+                    <Route path="/*" element={<Navigate to={"/home"} />} />
+                </Routes>
+            </div>
+        </>
+    );
 };
