b74a2744efdd3cedcae16f6d118ab51b84df338d
AlanMezva <L20170749@culiacan.tecnm.mx>
2024-09-14 17:14:52 -0700

    Changed some styles on navbar and sidebar

--- src/ui/components/Navbar.tsx
+++ src/ui/components/Navbar.tsx
@@ -1,19 +1,18 @@
-import { Bell, ChevronDown, CircleUser } from "lucide-react";
-import { SearchHere } from "./SearchHere";
+import {Bell, ChevronDown, CircleUser} from "lucide-react";
+import {SearchHere} from "./SearchHere";
 import style from "../style/navbar.module.css";
 
 export const Navbar = () => {
-    return (
-        <section className={`${style.navbar}`}>
-            <SearchHere />
-            <div className={`${style.navbarIcons}`}>
-                <Bell />
+	return (
+		<section className={`${style.navbar}`}>
+			<SearchHere />
+			<div className={`${style.navbarIcons}`}>
+				<Bell size={28} />
 
-                <div>
-                    <CircleUser />
-                    <ChevronDown />
-                </div>
-            </div>
-        </section>
-    );
+				<CircleUser size={34} />
+
+				<ChevronDown size={24} />
+			</div>
+		</section>
+	);
 };
