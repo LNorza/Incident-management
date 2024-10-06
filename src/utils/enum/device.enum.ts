4ecfc94e456160ec22b6ddaa38e3cc53e36b9238
AlanMezva <L20170749@culiacan.tecnm.mx>
2024-10-06 16:54:49 -0700

    Added devices interfaces

--- src/utils/enum/device.enum.ts
+++ src/utils/enum/device.enum.ts
@@ -1,9 +1,86 @@
 export type DeviceState = 'Activo' | 'En reparación' | 'Inactivo'
 
 export interface Device {
-    _id: string
     name: string
     type: string
+    status: string
+    specs: IComputerSpecs | ILaptop | IPrinter | ISwitch | IRouter | INoBreak | IVoltageRegulator | IProjector
+    purchaseDate: Date
+    warrantyYears: number
+    deviceModel: string
     brand: string
-    specs: []
+    location_id: string
+}
+
+export interface IComputerSpecs {
+    os?: string
+    motherboard?: string
+    cpu?: string
+    gpu?: string
+    ram?: string
+    ramType?: string
+    storage?: string
+    powerSupply?: string
+    ipAddress?: string
+    macAddress?: string
+    connectedPort?: string
+    isShared?: boolean
+    user_id?: string
+}
+
+export interface ILaptop {
+    os?: string
+    storage?: string
+    cpu?: string
+    gpu?: string
+    ram?: string
+    ramType?: string
+    wifiConnection?: boolean
+    ipAddress?: string
+    macAddress?: string
+    connectedPort?: string
+    user_id?: string
+}
+
+export interface IPrinter {
+    printerType?: string
+    tonerType?: string
+    printerInk?: string
+    scanner?: boolean
+    wifiConnection?: boolean
+    ipAddress?: string
+    macAddress?: string
+}
+
+export interface ISwitch {
+    ports?: number
+    macAddress?: string
+}
+
+export interface IRouter {
+    routerType?: string
+    ipAddress?: string
+    macAddress?: string
+    ports?: number
+    connectivity?: string
+    capacity?: string
+}
+
+export interface INoBreak {
+    powerCapacity?: string
+    ports?: number
+    backupTime?: string
+}
+
+export interface IVoltageRegulator {
+    powerCapacity?: string
+    ports?: number
+}
+
+export interface IProjector {
+    resolution?: string
+    connectivity?: string
+    brightness?: string
+    scope?: string
+    control?: boolean
 }
