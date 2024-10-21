955d0bc63b5c6336f925db116c292cfc30631bf8
LNorza <luiscnorza@gmail.com>
2024-10-21 16:16:06 -0700

    commit temporal

--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -3,7 +3,7 @@ import { useState } from 'react'
 export const useForm = <T extends object>(initialForm: T) => {
   const [formState, setFormState] = useState<T>(initialForm)
 
-  const onInputChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+  const onInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target
     setFormState((prevState) => ({
       ...prevState,
@@ -15,6 +15,7 @@ export const useForm = <T extends object>(initialForm: T) => {
     setFormState(initialForm)
   }
 
+  // Función para actualizar campos específicos
   const updateFields = (updatedFields: Partial<T>) => {
     setFormState((prevState) => ({
       ...prevState,
--- src/ui/components/CustomInput.tsx
+++ src/ui/components/CustomInput.tsx
@@ -1,4 +1,4 @@
-import React, { InputHTMLAttributes, TextareaHTMLAttributes } from 'react'
+import React, { InputHTMLAttributes } from 'react'
 import styles from '../style/customInput.module.css'
 
 interface Props extends InputHTMLAttributes<HTMLInputElement> {
@@ -7,26 +7,16 @@ interface Props extends InputHTMLAttributes<HTMLInputElement> {
     placeholder?: string
     type?: string
     value?: string
-    onChange: (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
     autocomplete?: string
     isFormInput?: boolean
 }
 
-export const CustomInput = ({ icon, type, isFormInput, ...props }: Props) => {
+export const CustomInput = ({ icon, isFormInput, ...props }: Props) => {
     return (
-        <>
-            {type === 'description' ? (
-                <textarea
-                    {...(props as TextareaHTMLAttributes<HTMLTextAreaElement>)}
-                    className={`${styles.textarea} ${isFormInput ? styles.form : ''}`}
-                    onChange={(e) => props.onChange(e as React.ChangeEvent<HTMLTextAreaElement>)} // Llamada a onChange general
-                />
-            ) : (
-                <div className={` ${styles.inputContainer}  ${icon ? styles.icon : ''}`}>
-                    {icon && <span className={`${styles.inputIcon} ${icon ? styles.icon : ''}`}>{icon}</span>}
-                    <input {...props} className={`${styles.inputField} ${isFormInput ? styles.form : ''}`} />
-                </div>
-            )}
-        </>
+        <div className={` ${styles.inputContainer}  ${icon ? styles.icon : ''}`}>
+            {icon && <span className={`${styles.inputIcon} ${icon ? styles.icon : ''}`}>{icon}</span>}
+            <input {...props} className={`${styles.inputField} ${isFormInput ? styles.form : ''}`} />
+        </div>
     )
 }
